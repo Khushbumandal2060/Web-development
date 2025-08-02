62112eb7c4c9f6f9292e0bbc563eb158c8bdb84a
khushi <khusbumandal2060@gmail.com>
2025-08-02 21:33:41 +0545

    First program of JS

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,11 +1,11 @@
 alert("Hello World");
 
 console.log("Code is running...")
-console.log("Code is also running...")
+console.log("Code is again running...")
 console.log("Code is looking like a wow...")
 
-var a = prompt("Enter your number")
-var isTrue = confirm("Are you sure you want to leave this page and blast your computer ")
+var a = prompt("Enter your number");
+var isTrue = prompt("Are you sure you want to leave this page and blas your computer ")
 
 if(isTrue){
     console.log("Computer is blasting")
@@ -13,10 +13,7 @@ if(isTrue){
 
 else{
     console.log("Computer is not blasting")
-
 }
+
 console.log("Your number is " + a)
-
-document.title = "Hey I am good"
-
 // document.body.style.backgroundColor = "red"
